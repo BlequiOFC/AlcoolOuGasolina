0631a0da36911ac7e1b18354ca103adb09b6bb19
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-02 20:11:56 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+decimal gasolina, etanol, dif;
+string recomend;
+
+Console.WriteLine("--- Etanol ou Gasolina? ---\n");
+
+Console.Write("Digite o preço do etanol (R$).....: ");
+etanol = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o preço da gasolina (R$)...: ");
+gasolina = Convert.ToDecimal(Console.ReadLine());
+
+dif = (etanol * 100) / gasolina;
+
+Console.WriteLine($"O preço do etanol corresponde a {dif:N1}% do preço da gasolina.");
+
+if (dif > 73)
+{
+    recomend = "Gasolina";
+}
+else
+{
+    recomend = "Etanol";
+}
+
+Console.WriteLine($"Recomendação: Abasteça com {recomend.ToUpper()}");
